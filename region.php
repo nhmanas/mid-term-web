e127988dbb8cd8d4d51c3a8cea20a3a110d0ad1c
Nhmanas <nhmanas003@gmail.com>
2018-06-03 04:22:28 +0300

    sayfalara css uyarlandi ve eklendi

--- region.php
+++ region.php
@@ -1,8 +1,60 @@
 <html>
     <head>
         <title>Pre-order</title>
+        <style>
+            .button {
+              background-color: #4CAF50; /* Green */
+              border: none;
+              color: white;
+              padding: 4px 15px;
+              text-align: center;
+              text-decoration: none;
+              display: inline-block;
+              -webkit-transition-duration: 0.4s; /* Safari */
+              font-size: 14 px;
+            }
+            .button1 {
+                background-color: white; 
+                color: black; 
+                border: 2px solid #4CAF50;
+            }
+
+            .button1:hover {
+                background-color: #4CAF50;
+                color: white;
+            }
+            .button2 {
+              background-color: white; 
+              color: black; 
+              border: 2px solid #008CBA;
+            }
+
+            .button2:hover {
+                background-color: #008CBA;
+                color: white;
+            }
+            #bg {
+                position: fixed;
+                right: 0;
+                bottom: 0;
+                min-width: 100%; 
+                min-height: 100%;
+                z-index: -1;
+            }
+            .content {
+                position: fixed;
+                bottom: 0;
+                background: rgba(0, 0, 0, 0.5);
+                color: #f1f1f1;
+                width: 100%;
+                padding: 50px;
+                z-index: 2;
+                font-family: "Verdana"
+            }
+        </style>
     </head>
-    <body>
+    <body background="pizza.jpg" style="background-repeat: no-repeat; background-size: cover;">
+        <div class="content">
         <p>Welcome to CSE216 pizza ordering website. Please choose your region and type of ordering.</p>
         <p>
             Regions:
@@ -16,8 +68,9 @@
                 <input type="checkbox" name="orderType[]" value="pizza"> Pizza
                 <input type="checkbox" name="orderType[]" value="beverage"> Beverage
                 <input type="checkbox" name="orderType[]" value="dessert"> Dessert <br><br>
-                <button name="next" id="next" type="submit">Next</button>
+                <button class="button button1" name="next" id="next" type="submit">Next</button>
             </form>
         </p>
+        </div>
     </body>
 </html>
